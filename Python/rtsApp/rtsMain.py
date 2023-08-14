59eda85b23843c8c65d80bae839b44aefc51acdf
RhyanLucca <95009088+RhyanLucca@users.noreply.github.com>
2023-08-14 18:01:25 -0300

    Update rtsMain.py

--- Python/rtsApp/rtsMain.py
+++ Python/rtsApp/rtsMain.py
@@ -1,6 +1,9 @@
 import customtkinter as ctk
 from tkinter import *
+from tkinter import ttk
 from PIL import Image
+import mysql.connector
+from mysql.connector import Error
 
 app = ctk.CTk()
 
@@ -15,6 +18,14 @@ class Aplication():
         self.currentFrame = ""
         self.app.mainloop()
 
+        self.con = mysql.connector.connect(
+        user= 'root',
+        password= '@RhyanLucca1000',
+        host= 'localhost',
+        database= 'RTSystems'
+        )
+
+        self.cursor = self.con.cursor()
 
     def tela_inicial(self):
         self.currentFrame = "FrameInicial"
@@ -154,14 +165,139 @@ class Aplication():
         ctk.CTkLabel(master=self.frameEstoque, text="Estoque", font=(f"{self.font}", self.scr_width/30)).place(relx=0.1, rely=0.05)
 
         self.valor= ''
-        
+
         def fornecedores_crud():
+
+            self.con = mysql.connector.connect(
+            user= 'root',
+            password= '@RhyanLucca1000',
+            host= 'localhost',
+            database= 'RTSystems'
+            )
+
+            self.cursor = self.con.cursor()
+
+            self.FrameSelecinado.pack_forget()
+            def gravar():
+                if CodigoEntry.get() == "": #or  nameEntry.get=="" or ContatoEntry.get() == "" or enderecoEntry.get() == "":
+                    print("erro")
+                else:
+                    tv.insert("", "end", values=(CodigoEntry.get(), nameEntry.get(), ContatoEntry.get(), enderecoEntry.get() ))
+                    CodigoEntry.delete(0, END)
+                    nameEntry.delete(0, END)
+                    ContatoEntry.delete(0, END) 
+                    enderecoEntry.delete(0, END)
+
+
+            def deletar():
+                try:
+                    itemSelecionado = tv.selection()[0]
+                    tv.delete(itemSelecionado)
+                except:
+                    print("Selecione um item válido")
+
+            
+            def obter():
+                try:
+                    itemSelecionado = tv.selection()[0]
+                    valores = tv.item(itemSelecionado, "values")
+                    print(valores)
+                except:
+                    print("Selecione um item válido")
+
+
+
+            frameCrud = ctk.CTkFrame(master=self.frameGeral, width=(self.scr_width)-(self.scr_width/100)*500, corner_radius=False, border_color="black", border_width=1)
+            frameCrud.pack(fill='both', side="right", expand=True)
+            self.frame_menu_lateral()
+            self.FrameSelecinado = frameCrud
+
+            ctk.CTkLabel(master=self.FrameSelecinado, text="Fornecedores", font=(f"{self.font}", self.scr_width/30)).place(relx=0.1, rely=0.05)
+
+
+            ctk.CTkLabel(master=self.FrameSelecinado, text="Código do Fornecedor *", font=(f"{self.font}", self.scr_width/50)).place(relx=0.1, rely=0.15)
+            CodigoEntry = ctk.CTkEntry(master=self.FrameSelecinado, placeholder_text="Código do fornecedor", width=(self.scr_width/100)*40, font=(f"{self.font}", self.scr_width/50))
+            CodigoEntry.place(relx=0.1, rely=0.2)
+
+            ctk.CTkLabel(master=self.FrameSelecinado, text="Nome do Fornecedor *", font=(f"{self.font}", self.scr_width/50)).place(relx=0.1, rely=0.25)
+            nameEntry = ctk.CTkEntry(master=self.FrameSelecinado, placeholder_text="Nome do fornecedor", width=(self.scr_width/100)*40, font=(f"{self.font}", self.scr_width/40))
+            nameEntry.place(relx=0.1, rely=0.30)
+
+            ctk.CTkLabel(master=self.FrameSelecinado, text="Contato do Fornecedor", font=(f"{self.font}", self.scr_width/50)).place(relx=0.1, rely=0.35)
+            ContatoEntry = ctk.CTkEntry(master=self.FrameSelecinado, placeholder_text="Contato do fornecedor", width=(self.scr_width/100)*40, font=(f"{self.font}", self.scr_width/40))
+            ContatoEntry.place(relx=0.1, rely=0.4)
+
+            ctk.CTkLabel(master=self.FrameSelecinado, text="E-mail do Fornecedor", font=(f"{self.font}", self.scr_width/50)).place(relx=0.1, rely=0.45)
+            emailEntry = ctk.CTkEntry(master=self.FrameSelecinado, placeholder_text="email do fornecedor", width=(self.scr_width/100)*40, font=(f"{self.font}", self.scr_width/40))
+            emailEntry.place(relx=0.1, rely=0.5)
+
+            ctk.CTkLabel(master=self.FrameSelecinado, text="Contato do Fornecedor", font=(f"{self.font}", self.scr_width/50)).place(relx=0.1, rely=0.55)
+            enderecoEntry = ctk.CTkEntry(master=self.FrameSelecinado, placeholder_text="Endereço do fornecedor", width=(self.scr_width/100)*40, font=(f"{self.font}", self.scr_width/40))
+            enderecoEntry.place(relx=0.1, rely=0.6)
+
+            #ctk.CTkLabel(master=self.FrameSelecinado, text="Os campos com * são obrigatórios", font=(f"{self.font}", self.scr_width/60)).place(relx=0.1, rely=0.65)
+
+            tv = ttk.Treeview(master=self.FrameSelecinado, columns=("id", "codigo", "nome", "contato", "endereco"), show="headings")
+            #tv['columns']=('Rank', 'Name', 'Badge')
+            tv.column('id',  width=round((self.scr_width/100)*3), stretch=NO)
+            tv.column('codigo', width=round((self.scr_width/100)*5), anchor=CENTER)
+            tv.column('nome', width=round((self.scr_width/100)*15), anchor=CENTER)
+            tv.column('contato', width=round((self.scr_width/100)*7), anchor=CENTER)
+            tv.column('endereco', width=round((self.scr_width/100)*13), anchor=CENTER)
+
+            tv.heading('id', text='ID', anchor=CENTER)
+            tv.heading('codigo', text='Codigo', anchor=CENTER)
+            tv.heading('nome', text='Nome', anchor=CENTER)
+            tv.heading('contato', text='Contato', anchor=CENTER)
+            tv.heading('endereco', text='Endereço', anchor=CENTER)
+
+            fornecedoresQueryREAD = "SELECT * FROM fornecedores"
+
+            self.cursor.execute(fornecedoresQueryREAD)
+
+            #lista = [["1", "Rhyan", "11985987199", "Rua belem 68 ap 13"],["2", "Rhyan", "11985987199", "Rua belem 68 ap 13"],["3", "Rhyan", "11985987199", "Rua belem 68 ap 13"]]
+            lista = self.cursor.fetchall()
+
+            for r in lista:
+                
+                idFornec = r[0]
+                codFornec = r[1]
+                nomeFornec = r[2]
+                contFornec = r[3]
+                emailFornec = r[4]
+                endFornec= r[5]
+                tv.insert(parent='', index="end", text='', values=(idFornec, codFornec, nomeFornec, contFornec, emailFornec, endFornec))
+                
+
+                #print(lista[4])
+
+
+            #for (id, nome, fone, end) in lista:
+            #    tv.insert(parent='', index="end", text='', values=(id, nome,fone, end))
+
+            # tv.insert(parent='', index=1, iid=1, text='', values=('2','Anil','Bravo'))
+            # tv.insert(parent='', index=2, iid=2, text='', values=('3','Vinod','Charlie'))
+            # tv.insert(parent='', index=3, iid=3, text='', values=('4','Vimal','Delta'))
+            # tv.insert(parent='', index=4, iid=4, text='', values=('5','Manjeet','Echo'))
+
+            tv.place(relx=0.53, rely=0.15, height=(self.scr_height/100) * 49)
+
+            btnSalvar = ctk.CTkButton(master=self.FrameSelecinado, text="Gravar", command=gravar, width=(self.scr_width/100)*17, font=(f"{self.font}", self.scr_width/40))
+            btnSalvar.place(relx=0.1, rely=0.85)
+            btnVisualizar = ctk.CTkButton(master=self.FrameSelecinado, text="Obter", command=obter, width=(self.scr_width/100)*17, font=(f"{self.font}", self.scr_width/40))
+            btnVisualizar.place(relx=0.329, rely=0.85)
+            #btnAtualizar = ctk.CTkButton(master=self.FrameSelecinado, text="Editar", width=(self.scr_width/100)*17, font=(f"{self.font}", self.scr_width/40)).place(relx=0.55, rely=0.85)
+            btnDeletar = ctk.CTkButton(master=self.FrameSelecinado, text="Excluir", command=deletar, width=(self.scr_width/100)*17, font=(f"{self.font}", self.scr_width/40))
+            btnDeletar.place(relx=0.75, rely=0.85)
+
+
+        def produtos_crud():
 
             frameCrud = ctk.CTkFrame(master=self.frameEstoque, width=(self.scr_width)-(self.scr_width/100)*500, corner_radius=False, border_color="black", border_width=1)
             frameCrud.pack(fill='both', side="right", expand=True)
             self.frame_menu_lateral()
-            ctk.CTkLabel(master=frameCrud, text="Fornecedores", font=(f"{self.font}", self.scr_width/30)).place(relx=0.1, rely=0.05)
-            
+            ctk.CTkLabel(master=frameCrud, text="Produtos", font=(f"{self.font}", self.scr_width/30)).place(relx=0.1, rely=0.05)
+
             CodigoEntry = ctk.CTkEntry(master=frameCrud, placeholder_text="Código do fornecedor", width=(self.scr_width/100)*40, font=(f"{self.font}", self.scr_width/40))
             CodigoEntry.place(relx=0.1, rely=0.2)
 
@@ -174,14 +310,6 @@ class Aplication():
             enderecoEntry = ctk.CTkEntry(master=frameCrud, placeholder_text="Endereço do fornecedor", width=(self.scr_width/100)*40, font=(f"{self.font}", self.scr_width/40))
             enderecoEntry.place(relx=0.1, rely=0.65)
 
-
-        def produtos_crud():
-
-            frameCrud = ctk.CTkFrame(master=self.frameEstoque, width=(self.scr_width)-(self.scr_width/100)*500, corner_radius=False, border_color="black", border_width=1)
-            frameCrud.pack(fill='both', side="right", expand=True)
-            self.frame_menu_lateral()
-            ctk.CTkLabel(master=frameCrud, text="Produtos", font=(f"{self.font}", self.scr_width/30)).place(relx=0.1, rely=0.05)
-
         def analEstoque_crud():
 
             frameCrud = ctk.CTkFrame(master=self.frameEstoque, width=(self.scr_width)-(self.scr_width/100)*500, corner_radius=False, border_color="black", border_width=1)
@@ -192,8 +320,6 @@ class Aplication():
         fornecBtn = ctk.CTkButton(master=self.frameEstoque, text="Fornecedores", command=fornecedores_crud, width=(self.scr_width/100)*35, font=(f"{self.font}", self.scr_width/30)).place(relx=0.5, rely=0.25, anchor=CENTER)
         prodBtn = ctk.CTkButton(master=self.frameEstoque, text="Produtos", command=produtos_crud, width=(self.scr_width/100)*35, font=(f"{self.font}", self.scr_width/30)).place(relx=0.5, rely=0.45,anchor=CENTER)
         analEstoqueBtn = ctk.CTkButton(master=self.frameEstoque, text="Analisar Estoque", command=analEstoque_crud, width=(self.scr_width/100)*35, font=(f"{self.font}", self.scr_width/30)).place(relx=0.5, rely=0.65,anchor=CENTER)
-
-
 
     def vendas_frame(self):
         
